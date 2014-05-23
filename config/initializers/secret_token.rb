6f6fbe4c5ee24da555a97f7ba036ae8283474762
jpramos <justin.ramos21@gmail.com>
2014-05-23 16:14:21 -0500

    Restructuring folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeColectif::Application.config.secret_key_base = 'b1717c59eff7fe174d338af2c33bdaff9d7e3f1c8dcfde769daf5fd743d4a21c0f4a82f3b10ee81f5d8a458af1e896e69899e2c0be91fe69d8a1a49c3feab595'
