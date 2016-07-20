82247389755c1a9b169e5eece23e0f6367e256a2
Justin Ramos <jramos@jramos.local>
2016-07-20 15:22:57 -0400

    Remove unused code. Specify Ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,4 @@ ActiveRecord::Schema.define(version: 20140514025826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
